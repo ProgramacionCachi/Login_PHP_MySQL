e8513b3f82fefaed5a09f632ab1440562e6bf5e4
Programación con Cachi <100305118+ProgramacionCachi@users.noreply.github.com>
2022-11-07 16:09:30 -0600

    Se agrega un comentario para pruebas de GitHub

--- Login/registro.php
+++ Login/registro.php
@@ -10,7 +10,7 @@
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script src="js/jquery-1.12.4-jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
-<h1>Hola Mundo para pruebas de GitHub</h1>
+<h1>Hola Mundo para pruebas de GitHub</h1> <!-- Comentario para prueba de GitHub --->
 </head>
 	<body>
 <?php
